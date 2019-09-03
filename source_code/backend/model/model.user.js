c2f4ccb834c3d4648d100b178fc2926f749b46c3
Rayaan Ahmed <raybox94@gmail.com>
2019-09-03 13:17:49 -0500

    Add user model

---
+++ source_code/backend/model/model.user.js
@@ -0,0 +1,63 @@
+var mongoose = require('mongoose');
+var validator = require('validator');
+
+var UserModel = mongoose.model('UserModel', {
+    email_id: {
+        type: String,
+        required: true,
+        trim: true,
+        unique: true,
+        minlength: 5,
+        validate: {
+            validator: (value) => {
+                return validator.isEmail(value);
+            }
+        }
+    },
+    first_name: {
+        type: String
+    },
+    last_name: {
+        type: String
+    },
+    password: {
+        type: String,
+        require: true,
+        minlength: 6
+    },
+    role: {
+        type: String,
+        enum: ['admin', 'student', 'instructor'],
+        require: true
+    },
+    is_active: {
+        type: Boolean,
+        require: true,
+        default: false
+    },
+    last_login: {
+        type: Date,
+    },
+    course: [
+        {
+            course_id: { type: String },
+            isRevealed: { type: Boolean }
+        }
+
+    ],
+    codewordset: [
+        {
+            codewordsetname: { type: String }
+        }
+    ],
+    created_at: {
+        type: Date
+    },
+    updated_at: {
+        type: Date,
+    }
+
+
+
+});
+module.exports.UserModel = UserModel
