2be6313fe155a74265c932cc2fd54eb33400f1cf
Ankit Prakash <ankit.prakash687@gmail.com>
2019-09-03 13:52:09 -0500

    Add signup component and its route to frontend

--- source_code/frontend/src/App.js
+++ source_code/frontend/src/App.js
@@ -1,16 +1,30 @@
-import React from 'react';
+import React, { useState, Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import NavBar from './component/Navbar'
+import AppBar from './component/MyAppBar'
 import Login from './component/Login'
-import  { Redirect } from 'react-router-dom'
+import Dashboard from './component/Dashboard'
+import Course from './component/instructor/Course'
+import { BrowserRouter as Router, Route, Switch, Redirect } from "react-router-dom"
+import Signup from './component/Signup'
+//import history from './helper/history'
+import PrivateRoute from './component/PrivateRoute'
 
-export class App extends React.Component {
-  render() {
-    return (
-     <p> working</p>
-    );
-  }
+class App extends Component {
+
+ constructor(props){
+   super(props)
+ 
+ }
+
+  render(){
+  
+  return (
+    <Router >
+        <Route path="/signup" component={Signup} />
+    </Router>
+  );
 }
-
+}
 export default App;
--- source_code/frontend/src/index.js
+++ source_code/frontend/src/index.js
@@ -2,16 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import Signup from './component/Signup'
-import NavBar from './component/Navbar'
-import Login from './component/Login'
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom"
 import * as serviceWorker from './serviceWorker';
+import { Provider } from "react-redux";
+import store from "./store/index";
 
 ReactDOM.render((
-    <div>
-      <App></App>
-    </div>
+   <Provider store={store}>
+     <App/>
+   </Provider>
   ), document.getElementById('root'))
 
   
