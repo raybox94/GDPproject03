39ff8711b286ada02a40cab24bfccaa95496979c
Abhinay <abhinayreddy1278@gmail.com>
2019-09-03 21:45:52 -0500

    Change codeword.js with routes and pathnames

--- source_code/backend/codeword.js
+++ source_code/backend/codeword.js
@@ -5,10 +5,13 @@ var register= require('./DB_Operations/register');
 require('./config/database');
 //const formidable = require('express-formidable');
 var userRouter = require('./routes/user.route');
+var authRouter = require('./routes/auth.route')
 require('dotenv').config();
 var app = express();
+var router = express.Router();
 const bodyParser = require('body-parser');
-var tokencheck = require('./middleware/tokencheck');
+var usersController = require('./controllers/controller.user')
+
 
 // parse body params and attache them to req.body
 app.use(bodyParser.json());
@@ -22,7 +25,7 @@ app.use(express.static(path.join(__dirname, 'views/dist/codeword')));
 app.engine("html", require('ejs').renderFile)
 app.set('view engine', 'html');
 
-//app.use(tokencheck.tokencheck);
+
 app.use(express.json());
 
 
@@ -32,10 +35,10 @@ app.get("/",function(req,res){
 });
 
 
-// start Express app
-app.listen(app.get('port'), () => {
-   console.log('App is running at http://localhost:%d in %s mode', app.get('port'), app.get('env'))
-   console.log('  Press CTRL-C to stop\n')
- })
+router.post('/codeword/api/v1/signup', usersController.signUp);
+router.post('/codeword/api/v1/signin', usersController.signIn);
+app.use('/codeword/api/v1/dashboard', userRouter);
+app.use('/codeword/api/v1/auth', authRouter)
+
 
 module.exports = app;
