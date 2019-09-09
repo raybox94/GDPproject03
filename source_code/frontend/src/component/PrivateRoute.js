60b6c7406502b9b719322178d3d04f42ed829736
Rayaan Ahmed <raybox94@gmail.com>
2019-09-08 22:41:54 -0500

    Add privateroute component and its route

---
+++ source_code/frontend/src/component/PrivateRoute.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import AppBar from './MyAppBar'
+import { Route, Redirect, withRouter } from 'react-router-dom';
+
+const PrivateRoute = ({ component: Component, ...rest }) => (
+    <Route {...rest} render={props => (
+        sessionStorage.getItem('token')
+            ? <Component {...props} />
+            : <Redirect to={{ pathname: '/signin', state: { from: props.location } }} />
+    )} />
+)
+
+export default withRouter(PrivateRoute);
