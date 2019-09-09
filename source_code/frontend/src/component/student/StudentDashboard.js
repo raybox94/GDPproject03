3ae70a80c5ab8b80ed8d5f7e8529bb97f83fdd1b
rahul reddy <rahulreddy062@gmail.com>
2019-09-09 18:11:21 -0500

    Initialize the student dashboard components and properties

---
+++ source_code/frontend/src/component/student/StudentDashboard.js
@@ -0,0 +1,42 @@
+import Typography from '@material-ui/core/Typography';
+import React, { useState, Component, useEffect } from 'react';
+import PropTypes from 'prop-types';
+import { makeStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Tabs from '@material-ui/core/Tabs';
+import Tab from '@material-ui/core/Tab';
+import Box from '@material-ui/core/Box';
+import { withStyles } from '@material-ui/core/styles';
+import { green, lightGreen, red } from '@material-ui/core/colors';
+import { Paper, Grid } from '@material-ui/core';
+import DialogTitle from '@material-ui/core/DialogTitle';
+import Dialog from '@material-ui/core/Dialog';
+import Button from '@material-ui/core/Button'
+import API from '../../utils/API'
+import Card from './CourseCard'
+ 
+export default function StudentDashboard() {
+
+
+    const [value, setValue] = useState(0);
+    const [open, setOpen] = useState(false)
+
+    const classes = useStyles();
+    function TabPanel(props) {
+        const { children, value, index, ...other } = props;
+
+        return (
+            <Typography
+                component="div"
+                role="tabpanel"
+                hidden={value !== index}
+                id={`simple-tabpanel-${index}`}
+                aria-labelledby={`simple-tab-${index}`}
+                {...other}
+            >
+                <Box bgcolor={lightGreen[100]} height={500} p={3}>{children}</Box>
+            </Typography>
+        );
+    }
+
+}//Intialize the student dashboard properties and components.
