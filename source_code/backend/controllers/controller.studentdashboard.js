558c8447a46fcc801bf5ced0d0dda8c035b7d975
Abhinay <abhinayreddy1278@gmail.com>
2019-09-10 11:37:04 -0500

    Add get student details function

--- source_code/backend/controllers/controller.studentdashboard.js
+++ source_code/backend/controllers/controller.studentdashboard.js
@@ -3,3 +3,17 @@ var { mongoose } = require('./../config/database')
 var { CourseStudentModel } = require('../model/model.coursestudent');
 var express = require('express');
 var { CourseModel } = require('../model/model.course');
+
+let getstudentDetails = (req, res) => {
+    console.log("hhjkhjkhjkhjkh " + req.params.emailID);
+
+
+    CourseStudentModel.find({ EmailKey: req.params.emailID }, function (err, course) {
+        if (err) {
+
+            res.send(err)
+        }
+
+        return res.send(course);
+    });
+}
