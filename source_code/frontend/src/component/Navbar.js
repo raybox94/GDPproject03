c41412c6e6858da8db82392201a8266f0eeb6449
Rayaan Ahmed <raybox94@gmail.com>
2019-09-10 21:08:39 -0500

    Add navbar component

---
+++ source_code/frontend/src/component/Navbar.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+import AccountCircle from '@material-ui/icons/AccountCircle';
+import Switch from '@material-ui/core/Switch';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import FormGroup from '@material-ui/core/FormGroup';
+import MenuItem from '@material-ui/core/MenuItem';
+import Menu from '@material-ui/core/Menu';
+import purple from '@material-ui/core/colors/purple';
+import red from '@material-ui/core/colors/red';
+import Button from '@material-ui/core/Button';
+import PersonIcon from '@material-ui/icons/Person';
+
+const NavBar = (props) => {
+
+
+
+}
+
+export default NavBar;
