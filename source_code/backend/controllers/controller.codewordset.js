4b43b505bddaeacf30c249f7421560cd93a3bf2f
Ankit Prakash <ankit.prakash687@gmail.com>
2019-09-17 22:34:35 -0500

    Add function to update codewordset in the backend

--- source_code/backend/controllers/controller.codewordset.js
+++ source_code/backend/controllers/controller.codewordset.js
@@ -20,3 +20,50 @@ let addcodewordset = (req, res) => {
     })
 }
 module.exports.addcodewordset = addcodewordset;
+
+let upadatecodewordset = (req, res) => {
+    
+    var body = _.pick(req.body,['id','oldCodewordSetName','newCodewordSetName', 'codewords']);
+  
+    console.log(req.body)
+    console.log('**********update codewordset*******')
+    console.log(body)
+    if(body.codewords.length == 0){
+
+        Codewordset.findOne({'codewordSetName':body.newCodewordSetName}, (error, codewordSet) =>{
+            if(codewordSet){
+                return res.json({ code: 404, message: 'Codeword set exists' });
+            }else{
+                Codewordset.updateOne({'codewordSetName':body.oldCodewordSetName},
+                    {$set:{
+                        codewordSetName: body.newCodewordSetName
+                    }
+                }, (error, updatedCodewordSet)=>{
+                    if(error){
+
+                        return res.json({ code: 400, message: e });
+                    }
+                    console.log(updatedCodewordSet)
+                    return res.json({ code: 200, message: 'Codeword set updated' });
+                }) 
+            }
+        }
+        )
+          
+    }else{
+        Codewordset.updateOne({'codewordSetName':body.oldCodewordSetName},
+            {$set:{
+                codewordSetName: body.newCodewordSetName,
+                codewords: body.codewords
+            }
+        }, (error, updatedCodewordSet)=>{
+            if(error){
+                return res.json({ code: 400, message: e });
+            }
+            console.log(updatedCodewordSet)
+            return res.json({ code: 200, message: 'Codeword set updated' });
+        }) 
+    }
+ 
+}
+module.exports.upadatecodewordset = upadatecodewordset;
