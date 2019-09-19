5250e907989da84478553cbeb4c6fc0bd2584a1a
Ankit Prakash <ankit.prakash687@gmail.com>
2019-09-19 16:47:25 -0500

    Add function to generate report of codeword

--- source_code/backend/controllers/controller.codewordset.js
+++ source_code/backend/controllers/controller.codewordset.js
@@ -67,3 +67,91 @@ let upadatecodewordset = (req, res) => {
  
 }
 module.exports.upadatecodewordset = upadatecodewordset;
+
+let generateReport = (req, res) =>{
+
+    var body = _.pick(req.body,['id','level']);
+    console.log('********LEVEL**********')
+    console.log(body.level)
+    var similarityLevel
+    switch(body.level){
+        case 0:
+            similarityLevel = 0.3
+            break;
+        case 1:
+            similarityLevel = 0.5
+            break;
+        case 2:
+            similarityLevel = 0.7
+            break;
+        case 3:
+            similarityLevel = 0.8
+            break;
+        case 4:
+            similarityLevel = 0.9
+            break;
+        default:
+            similarityLevel = 0.5
+    }
+    console.log(similarityLevel)
+    Codewordset.findOne({_id: body.id}, (error, codewordset)=>{
+        if(error){
+            return res.json({ code: 400, message: error });
+        }
+
+        var codewords = codewordset.codewords
+        var result = []
+        //console.log(codewords)
+        for(var i in codewords){
+            var targetCodewords = codewords.filter((item, index)=>{
+                if(index != i){
+                    return item
+                }
+            })
+
+            result.push({word: codewords[i], similarity: stringSimilarity.findBestMatch(codewords[i], targetCodewords)})
+        }
+
+        var similars = []
+        var checkerArray = []
+       for(var i in result){
+
+        if(!checker(checkerArray, result[i].word)){
+          // console.log(result[i])
+           var ratings = result[i].similarity.ratings
+           var output = []
+           output.push(result[i].word)
+           for(var i in ratings){
+                if(ratings[i].rating > similarityLevel){
+                    output.push(ratings[i].target)
+                    checkerArray.push(ratings[i].target)
+                    }
+                }
+           }
+           
+           similars.push(output)
+        
+       }
+       let final = similars.filter((item)=>{
+           if(item.length > 1){
+               return item
+           }
+       })
+      // console.log(Array.from(new Set(final.map(JSON.stringify)), JSON.parse))
+     
+      
+     var anagrams = anagramFinder.find(codewords).filter((item)=>{
+         if(item.length > 1){
+             return item
+         }
+     })
+
+     var data ={similars: Array.from(new Set(final.map(JSON.stringify)), JSON.parse),
+                anagrams: anagrams}
+    // console.log('************ANAGRAMS******************')
+     //console.log(anagrams)
+      return res.json({ code: 200, data: data });
+    })
+  }
+
+  module.exports.generateReport = generateReport
