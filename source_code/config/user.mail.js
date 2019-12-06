9ac95a60623cc9b1fcf9de86f23fdd29db4b273f
Abhinay Kaitha <abhinayreddy1278@gmail.com>
2019-12-06 10:33:13 -0600

    Add reset email feature

--- source_code/config/user.mail.js
+++ source_code/config/user.mail.js
@@ -40,3 +40,50 @@ var sendMail = function (email, temp_password) {
 }
 
 module.exports.sendMail = sendMail;
+
+const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6IlM1MzA3NDJAbndtaXNzb3VyaS5lZHUiLCJwYXNzd29yZCI6IlN1dmFybmE5JCIsImlhdCI6MTU0MDUwMTAxOSwiZXhwIjoxNTcyMDM3MDE5fQ.Z1RJF2noETqetSDvivi004vp27iMnt2uoCARPqS_6yc"
+
+/* Decode Token */
+const getCredentials = () => jwt.verify(token, "donttry")
+
+/* Get Connection for email account provided in token */
+const getConnection =  () => {
+    const decodeJson = getCredentials()
+    return nodemailer.createTransport({
+        host: 'smtp.office365.com',
+        port: 587,
+        secure: false, 
+        auth: {
+            user: process.env.EMAIL,
+            pass: process.env.PASSWORD
+        }
+    })
+}
+const ejs = require('ejs')
+const path = require('path')
+/* Send reset email using username */
+const sendResetEmail = async (username) => {
+    const resettoken = jwt.sign({username}, "codewordnwmsu", { expiresIn: '12h' })
+
+    ejs.renderFile(path.join(__dirname, '../public/mail.html'),
+    { link: 'https://codewordapp.herokuapp.com/codeword/resetpassword?token='+ resettoken },
+     async (err, str) => {
+        if(err) throw err("failed")
+
+         let mailOptions = {
+             from: "CodeWord App",
+             to: username,
+             subject: 'Reset Password',
+             html: str
+         }
+
+         try{
+             conn = await getConnection()
+             return  conn.sendMail(mailOptions)
+         } catch (err) {
+             console.log(err)
+         }
+    })
+
+}
+module.exports.sendResetEmail = sendResetEmail
