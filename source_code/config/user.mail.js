338233246c4c1adb44b3a608198a281fc7ee705e
Abhinay Kaitha <abhinayreddy1278@gmail.com>
2019-12-06 10:30:33 -0600

    Initialize jwt verification

---
+++ source_code/config/user.mail.js
@@ -0,0 +1,27 @@
+const nodemailer = require('nodemailer');
+const _ = require('lodash');
+var jwt = require('jsonwebtoken');
+
+var sendMail = function (email, temp_password) {
+    var decodedvalue = "";
+    var decodeJson="";
+    nodemailer.createTestAccount((err, account) => {
+    var newToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6IlM1MzA3NDJAbndtaXNzb3VyaS5lZHUiLCJwYXNzd29yZCI6IlN1dmFybmE5JCIsImlhdCI6MTU0MDUwMTAxOSwiZXhwIjoxNTcyMDM3MDE5fQ.Z1RJF2noETqetSDvivi004vp27iMnt2uoCARPqS_6yc';
+
+    jwt.verify(newToken, 'donttry', (err, decoded, res) =>{
+        if(err)
+        {
+            console.log(err);
+        }
+        decodedvalue = JSON.stringify(decoded);
+        decodeJson = JSON.parse(decodedvalue);
+        
+        let transporter = nodemailer.createTransport({
+            host: 'smtp.office365.com',
+            port: 587,
+            secure: false, 
+            auth: {
+                user: decodeJson.email,
+                pass: decodeJson.password
+            }
+        });
